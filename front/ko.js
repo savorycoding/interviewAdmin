f4b2d30fcda1c9522e5d364e6ac92173a53fb120
savorycoding <54048700+savorycoding@users.noreply.github.com>
2019-11-25 11:20:28 +0900

    Update ko.js

--- front/ko.js
+++ front/ko.js
@@ -1,6 +1,7 @@
 let langData = {
   "header.label.home": "홈으로",
   "header.label.home2": "홈으로 ",
+  "header.label.home3": "홈으로 3",
   "header.label.exam_site": "응시자사이트",
   "header.label.greets": "000님 환영합니다.",
   "header.button.remain": "연장",
