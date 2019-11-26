96a98d8351384f26e09b4ffd0608c968ef313464
savorycoding <savorycode@midasit.com>
2019-11-26 14:32:42 +0900

    Automatic commit of updated project files

--- front/dest/ja.js
+++ front/dest/ja.js
@@ -395,7 +395,7 @@
   "regist.step5.mail.label.mapping6": "$受検サイト$",
   "regist.step5.mail.label.mapping7": "＄認証コード＄",
   "regist.step5.mail.guide.mapping": "マッピング項目を発表文に適用するためには、上のマッピング項目をクリックするか、直接タイピングしてください。",
-  "regist.step5.mail.guide.mapping2": "マッピング項目を上の方式でなく、コピーして貼る場合には正常的に働きません。",
+  "regist.step5.mail.guide.mapping2": "マッピングをコピー&ペーストする場合、正常に動作しません。",
   "regist.step5.mail.button.mail_preview": "先に見る",
   "regist.step5.mail.button.mail_testsend": "案内メール事前テスト",
   "regist.step5.modal_mail_preview.title": "案内メールのプレビュー",
